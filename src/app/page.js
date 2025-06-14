283d5dc5f84ced2de04045e207ce09bdf80c3b6a
Murat Aydın <mr.aydin@hotmail.com.tr>
2025-06-14 16:44:57 +0300

    auth3

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,22 @@
 "use client";
+export const dynamic = "force-dynamic";
+import { useSession, signOut } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
+const blogPosts = [
+	{
+		id: 1,
+		title: "İlk Blog Yazısı",
+		content: "Bu, örnek bir blog yazısıdır.",
+	},
+	{
+		id: 2,
+		title: "İkinci Yazı",
+		content: "İkinci yazının içeriği burada.",
+	},
+];
+
 export default function Home() {
   return <div>Merhaba, ana sayfa!</div>;
 }
